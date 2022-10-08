aca9106b1ac2ee8ac860d0ac1f0009280dc1d4ce
sinplx2015 <sinplx2015@gmail.com>
2022-10-08 19:56:32 +0800

    pca

--- demo6.py
+++ demo6.py
@@ -7,25 +7,23 @@ def PCA(data, dims_rescaled_data=2):
     R = np.cov(data, rowvar=False)
     evals, evecs = LA.eigh(R)
     print(evals, evecs)
+    
     idx = np.argsort(evals)[::-1]
-
     evecs = evecs[:,idx]
     evals = evals[idx]
     evecs = evecs[:, :dims_rescaled_data]
     
     return np.dot(evecs.T, data.T).T
 
-def plot_pca(data):
-    from matplotlib import pyplot as MPL
-    fig = MPL.figure()
-    ax1 = fig.add_subplot(111)
+def plot_pca(data,):
+    from matplotlib import pyplot as plt
     data_resc = PCA(data)
-    ax1.plot(data_resc[:, 0], data_resc[:, 1])
-    MPL.show()
+    print(data_resc)
+    plt.scatter(data_resc[:, 0], data_resc[:, 1] ,marker='.')
+    plt.show()
 
 def main():
-    data = np.loadtxt(r'C:\Users\Administrator\\Desktop\\2_Precast_Laminated_Panels.asc')
-    data = data[:,:3]
+    data = np.loadtxt(r'C:\Users\Administrator\\Desktop\\2_Precast_Laminated_Panels.asc', usecols=(0,1,2))
     plot_pca(data)
 
 if __name__ == '__main__':
